ef1ecc179ab3979e95303e7efd34eed6d2591b01
Rahul Agarwal <agarwalrahul@vmware.com>
2023-06-05 21:10:54 -0700

    simple openai chat example

--- hello-openai.py
+++ hello-openai.py
@@ -3,14 +3,8 @@ import openai
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-    )
-    print(response)
-    return response.choices[0].message["content"]
-
-print(get_completion("What would be good stocks to buy for a hypothetical high risk portfolio"))
+#chat completion example, multiple messages maybe sent
+messages = [{"role": "user", "content": "What would be good stocks to buy for a hypothetical high risk portfolio"}]
+response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                        messages=messages, temperature=0)
+print(response)
